974160dac81630155525b2ac236673f06ecca8aa
himanshu <himanshubnswl@gmail.com>
2025-09-04 23:23:33 +0530

    changed file directory a bit and added treeInternals, still have to test it but

---
+++ src/backend/TreeInternals.c
@@ -0,0 +1,123 @@
+//
+// Created by lhbdawn on 30-08-2025.
+//
+
+#include "TreeInternals.h"
+
+#include <stddef.h>
+
+Node * root = nullptr;
+
+void initialize(int data) {
+    root = (Node *)malloc(sizeof(Node));
+    root->data = data;
+    root->leftptr = nullptr;
+    root->rightptr = nullptr;
+
+}
+void addNode(int data) {
+    Node * iterateNode = root;
+    Node * parentNode;
+    if (iterateNode == nullptr) {
+        initialize(data);
+        return;
+    }
+    Node * newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->rightptr = nullptr;
+    newNode->leftptr = nullptr;
+
+    while (iterateNode != nullptr) {
+        if (iterateNode->data == data) {
+            free(newNode);
+            return;
+        }
+        parentNode = iterateNode;
+
+        if (data < iterateNode->data) {
+            iterateNode = iterateNode->leftptr;
+        }
+        else {
+            iterateNode = iterateNode->rightptr;
+        }
+    }
+    if (data < parentNode->data) {
+        parentNode->leftptr = newNode;
+    }
+    else {
+        parentNode->rightptr = newNode;
+    }
+}
+
+int removeNode(int valueToRemove) {
+    if (root == nullptr) {
+        return 1;
+    }
+    Node * currentNode = root;
+    Node * parentNode;
+
+    while (currentNode != nullptr && currentNode->data != valueToRemove) {
+        parentNode = currentNode;
+        if (valueToRemove < currentNode->data) {
+            currentNode = currentNode->leftptr;
+        }
+        else {
+            currentNode = currentNode->rightptr;
+        }
+    }
+    if (currentNode == nullptr) {
+        return 2;
+    }
+
+    if (currentNode->leftptr == nullptr && currentNode->rightptr == nullptr) {
+        if (parentNode->leftptr == currentNode) {
+            parentNode->leftptr = nullptr;
+            free(currentNode);
+        }
+        else {
+            parentNode->rightptr = nullptr;
+            free(currentNode);
+        }
+    }
+
+    else if (currentNode->rightptr == nullptr || currentNode->leftptr == nullptr) {
+        Node * childNode;
+        if (currentNode->rightptr != nullptr) {
+            childNode = currentNode->rightptr;
+        }
+        else {
+            childNode = currentNode->leftptr;
+        }
+        if (parentNode->leftptr == currentNode) {
+            parentNode->leftptr = childNode;
+            free(currentNode);
+        }
+        else {
+            parentNode->rightptr = childNode;
+            free(currentNode);
+        }
+    }
+
+    else {
+        Node * succesorNode = currentNode->rightptr;
+        Node * succesorParent = currentNode;
+
+        while (succesorNode->leftptr != nullptr) {
+            succesorParent = succesorNode;
+            succesorNode = succesorNode->leftptr;
+        }
+
+        currentNode->data = succesorNode->data;
+
+        if (succesorParent->leftptr == succesorNode) {
+            succesorParent->leftptr = succesorNode->rightptr;
+            free(succesorNode);
+        }
+        else {
+            succesorParent->rightptr = succesorNode->rightptr;
+            free(succesorNode);
+        }
+    }
+
+
+}
