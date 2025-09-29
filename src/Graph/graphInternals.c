b43502edbe3e20b84cf47332059f4e7908d67dd4
himanshu <himanshubnswl@gmail.com>
2025-09-29 23:43:21 +0530

    graph files added, work begins on a new and final page

---
+++ src/Graph/graphInternals.c
@@ -0,0 +1,3 @@
+//
+// Created by lhbdawn on 29-09-2025.
+//
---
+++ src/Graph/graphInternals.h
@@ -0,0 +1,10 @@
+//
+// Created by lhbdawn on 29-09-2025.
+//
+
+#ifndef GRAPHINTERNALS_H
+#define GRAPHINTERNALS_H
+struct graphNode {
+    int data;
+};
+#endif //GRAPHINTERNALS_H
