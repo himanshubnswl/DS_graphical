6619059d12ebb7dd38be0f56209938853b4c7a4d
himanshu <himanshubnswl@gmail.com>
2025-10-03 21:24:00 +0530

    started

--- src/Graph/graphInternals.c
+++ src/Graph/graphInternals.c
@@ -1,3 +1,38 @@
 //
 // Created by lhbdawn on 29-09-2025.
 //
+#include "graphInternals.h"
+
+Graph_Node ** adjacency_matrix = nullptr;
+
+void initialize_graph() {
+    adjacency_matrix = malloc(MAX_ROWS * sizeof(int *));
+    adjacency_matrix[0] = malloc(MAX_ROWS * MAX_COLUMNS * sizeof(int));
+
+    for (size_t i = 1; i < MAX_ROWS; i++) {
+        adjacency_matrix[i] = adjacency_matrix[0] + i * MAX_COLUMNS;
+    }
+}
+
+void free_adjacency_matrix() {
+    if (adjacency_matrix == nullptr) {
+        printf("\nmatrix hasn't been allocated!");
+        return;
+    }
+    free(adjacency_matrix[0]);
+    free(adjacency_matrix);
+}
+
+Graph_Node ** get_adjacencymatrix() {
+    if (adjacency_matrix != nullptr) {
+        return adjacency_matrix;
+    }
+    else {
+        printf("\ntrying to return a null adjacency matrix!");
+        return nullptr;
+    }
+}
+
+int add_graph_node() {
+
+}
--- src/Graph/graphInternals.h
+++ src/Graph/graphInternals.h
@@ -4,7 +4,17 @@
 
 #ifndef GRAPHINTERNALS_H
 #define GRAPHINTERNALS_H
-struct graphNode {
+#include <raylib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ROWS 50;
+#define MAX_COLUMNS 50;
+
+typedef struct graphNode {
     int data;
-};
+    int pos_x;
+    int pos_y;
+    Color color;
+}Graph_Node;
 #endif //GRAPHINTERNALS_H
